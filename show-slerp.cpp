3b854c5f7c28e5104344a4d0214b6b7aca5fe21e
Karl Yerkes <karl.yerkes@gmail.com>
2022-02-07 10:43:31 -0800

    improvements

--- show-slerp.cpp
+++ show-slerp.cpp
@@ -27,13 +27,12 @@ struct MyApp : App {
   void onCreate() override {
     randomize();
     nav().pos(3, 4, 10);
-    nav().faceToward(0, 0, 0);
+    nav().faceToward(Vec3d(0), Vec3d(0, 1, 0));
   }
 
   void onAnimate(double dt) override {
-
     // animation parameter used to rotate the drawing
-    angle += 0.87;
+    angle += 0.17;
 
     // animation parameter used to animate the wrawing and periodically reset
     time += dt;
@@ -48,7 +47,6 @@ struct MyApp : App {
   }
 
   void onDraw(Graphics &g) override {
-
     // graphics / drawing settings
     g.clear(1);
     g.meshColor();
@@ -67,19 +65,19 @@ struct MyApp : App {
       // p
       mesh.vertex(p.pos());
       mesh.color(1, 0, 0);
-      mesh.vertex(p.pos() + p.quat().rotate(Vec3d(0, 0, -1)));
+      mesh.vertex(p.pos() + p.quat().rotate(Vec3d(0, 0, -1)) * 0.3);
       mesh.color(1, 0, 0);
 
       // q
       mesh.vertex(q.pos());
       mesh.color(0, 0, 1);
-      mesh.vertex(q.pos() + q.quat().rotate(Vec3d(0, 0, -1)));
+      mesh.vertex(q.pos() + q.quat().rotate(Vec3d(0, 0, -1)) * 0.3);
       mesh.color(0, 0, 1);
 
       // w
       mesh.vertex(w);
       mesh.color(0, 1, 0);
-      mesh.vertex(w + u.rotate(Vec3d(0, 0, -1)));
+      mesh.vertex(w + u.rotate(Vec3d(0, 0, -1)) * 0.3);
       mesh.color(0, 1, 0);
 
       // reference frame
@@ -89,8 +87,7 @@ struct MyApp : App {
       mesh.vertex(0, 10, 0);
       mesh.vertex(0, 0, -10);
       mesh.vertex(0, 0, 10);
-      for (int i = 0; i < 6; i++)
-        mesh.color(0, 0, 0);
+      for (int i = 0; i < 6; i++) mesh.color(0, 0, 0);
 
       g.draw(mesh);
     }
