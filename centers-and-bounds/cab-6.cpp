59c0480547ec34b0097bb8f9674ea259a914ac87
Karl Yerkes <karl.yerkes@gmail.com>
2022-02-11 09:52:15 -0800

    after class additions

--- centers-and-bounds/cab-6.cpp
+++ centers-and-bounds/cab-6.cpp
@@ -6,11 +6,15 @@
 #include "al/app/al_App.hpp"
 #include "al/app/al_GUIDomain.hpp"
 #include "al/graphics/al_Shapes.hpp"
+#include "al/math/al_Functions.hpp"
 #include "al/math/al_Random.hpp"
 
 using namespace al;
 
 const int N = 100;
+
+// defined later
+void addBox(Mesh& mesh, const Vec3f& minimum, const Vec3f& maximum);
 
 class MyApp : public App {
  public:
@@ -50,8 +54,12 @@ class MyApp : public App {
     Vec3f sum(0, 0, 0);
     minimum = Vec3f(3e8);
     maximum = Vec3f(-3e8);
+
     for (int i = 0; i < N; i++) {
       local[i] = false;
+
+      if (i == index) continue;
+
       if ((point - position[i]).mag() < radius) {
         local[i] = true;     // mark this as "in the local flock"
         sum += position[i];  // find the center
@@ -66,7 +74,38 @@ class MyApp : public App {
         if (position[i].z < minimum.z) minimum.z = position[i].z;
       }
     }
-    center = sum / count;  // what if count == 0?
+    if (count > 0) {
+      center = sum / count;  // what if count == 0?
+    }
+
+    //
+    // Camera/Nav automation ~ We use the details we just computed (center and
+    // bounds) to point the camera and move the camera to that we can look at
+    // particular agents and their local groups. This code is not carefully
+    // explained; It also is far from perfect.
+    //
+
+    nav().smooth(0.5);
+    Vec3f middle = (minimum + maximum) / 2;
+    nav().faceToward(middle, Vec3f(0, 1, 0), 0.07);
+    nav().moveR(0.007);
+
+    {
+      Vec3f v(nav().pos());
+      float distance = (v - middle).mag();
+      float hueristic = al::clip((maximum - minimum).mag(), 3.0f, 0.3f);
+      if (distance > hueristic * 2.7) {
+        nav().nudgeToward(middle, 0.02);
+      } else if (distance < hueristic * 1.2) {
+        nav().nudgeToward(middle, -0.37);
+      }
+      v.y = middle.y;
+      distance = (v - nav().pos()).mag();
+      hueristic = 0.7;
+      if (distance > hueristic) {
+        nav().nudgeToward(v, 0.01);
+      }
+    }
   }
 
   void onDraw(Graphics& g) override {
@@ -114,34 +153,7 @@ class MyApp : public App {
     g.lighting(false);
 
     Mesh box(Mesh::LINES);
-
-    box.vertex(minimum.x, minimum.y, minimum.z);
-    box.vertex(maximum.x, minimum.y, minimum.z);
-    box.vertex(minimum.x, minimum.y, maximum.z);
-    box.vertex(maximum.x, minimum.y, maximum.z);
-    box.vertex(minimum.x, maximum.y, maximum.z);
-    box.vertex(maximum.x, maximum.y, maximum.z);
-    box.vertex(minimum.x, maximum.y, minimum.z);
-    box.vertex(maximum.x, maximum.y, minimum.z);
-
-    box.vertex(minimum.x, minimum.y, minimum.z);
-    box.vertex(minimum.x, maximum.y, minimum.z);
-    box.vertex(minimum.x, minimum.y, maximum.z);
-    box.vertex(minimum.x, maximum.y, maximum.z);
-    box.vertex(maximum.x, minimum.y, maximum.z);
-    box.vertex(maximum.x, maximum.y, maximum.z);
-    box.vertex(maximum.x, minimum.y, minimum.z);
-    box.vertex(maximum.x, maximum.y, minimum.z);
-
-    box.vertex(minimum.x, minimum.y, minimum.z);
-    box.vertex(minimum.x, minimum.y, maximum.z);
-    box.vertex(minimum.x, maximum.y, minimum.z);
-    box.vertex(minimum.x, maximum.y, maximum.z);
-    box.vertex(maximum.x, maximum.y, minimum.z);
-    box.vertex(maximum.x, maximum.y, maximum.z);
-    box.vertex(maximum.x, minimum.y, minimum.z);
-    box.vertex(maximum.x, minimum.y, maximum.z);
-
+    addBox(box, minimum, maximum);
     g.color(1);
     g.draw(box);
   }
@@ -155,3 +167,32 @@ class MyApp : public App {
 };
 
 int main() { MyApp().start(); }
+
+void addBox(Mesh& mesh, const Vec3f& minimum, const Vec3f& maximum) {
+  mesh.vertex(minimum.x, minimum.y, minimum.z);
+  mesh.vertex(maximum.x, minimum.y, minimum.z);
+  mesh.vertex(minimum.x, minimum.y, maximum.z);
+  mesh.vertex(maximum.x, minimum.y, maximum.z);
+  mesh.vertex(minimum.x, maximum.y, maximum.z);
+  mesh.vertex(maximum.x, maximum.y, maximum.z);
+  mesh.vertex(minimum.x, maximum.y, minimum.z);
+  mesh.vertex(maximum.x, maximum.y, minimum.z);
+
+  mesh.vertex(minimum.x, minimum.y, minimum.z);
+  mesh.vertex(minimum.x, maximum.y, minimum.z);
+  mesh.vertex(minimum.x, minimum.y, maximum.z);
+  mesh.vertex(minimum.x, maximum.y, maximum.z);
+  mesh.vertex(maximum.x, minimum.y, maximum.z);
+  mesh.vertex(maximum.x, maximum.y, maximum.z);
+  mesh.vertex(maximum.x, minimum.y, minimum.z);
+  mesh.vertex(maximum.x, maximum.y, minimum.z);
+
+  mesh.vertex(minimum.x, minimum.y, minimum.z);
+  mesh.vertex(minimum.x, minimum.y, maximum.z);
+  mesh.vertex(minimum.x, maximum.y, minimum.z);
+  mesh.vertex(minimum.x, maximum.y, maximum.z);
+  mesh.vertex(maximum.x, maximum.y, minimum.z);
+  mesh.vertex(maximum.x, maximum.y, maximum.z);
+  mesh.vertex(maximum.x, minimum.y, minimum.z);
+  mesh.vertex(maximum.x, minimum.y, maximum.z);
+}
